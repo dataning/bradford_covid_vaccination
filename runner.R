ccde1f6441bf00792b39d69431e2feb735c29c1d
dataning <datalulu@gmail.com>
2021-03-07 22:36:04 +0000

    commit

---
+++ runner.R
@@ -0,0 +1,53 @@
+pacman::p_load(tidyverse, data.table, workflowr, usethis, here,
+               httr, jsonlite, rvest, stringr)
+usethis::use_git()
+usethis::use_github()
+##%######################################################%##
+#                                                          #
+####                  Workflow set up                   ####
+#                                                          #
+##%######################################################%##
+
+wflow_start(here(), existing = TRUE)
+
+usethis::edit_file('code/spatio_reasons.R')
+
+wflow_build()
+
+wflow_publish(c("analysis/index.Rmd", 
+                "analysis/about.Rmd", 
+                "analysis/license.Rmd"), 
+              "first_try")
+
+##%######################################################%##
+#                                                          #
+####                  New beginning                     ####
+#                                                          #
+##%######################################################%##
+
+
+wflow_open("analysis/data_job_findajob.Rmd")
+
+usethis::edit_file('code/mexico.R')
+
+wflow_build()
+
+wflow_publish(c("analysis/data_job_findajob.Rmd", 
+                "code/data_job_findajob.R"), 
+              "first_try")
+
+##%######################################################%##
+#                                                          #
+####                     Automation                     ####
+#                                                          #
+##%######################################################%##
+
+usethis::edit_file('code/automation_runner.R')
+
+##%######################################################%##
+#                                                          #
+####                    Git one line                    ####
+#                                                          #
+##%######################################################%##
+
+git add . && git commit -a -m "commit" && git push
