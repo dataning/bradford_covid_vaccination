8fcd85b2c0a87896025f4003542a2a010fb7efb6
dataning <datalulu@gmail.com>
2021-03-09 15:35:02 +0000

    commit

--- code/map.R
+++ code/map.R
@@ -16,7 +16,7 @@ msoa_map <- df_msoa %>%
 
 # Check missing data by variables
 naniar::gg_miss_var(msoa_map)
-glimpse(msoa_map)
+View(msoa_map)
 
 palette = "YlOrRd", n = 4, contrast = c(0, 1)
 style ="cont", palette = "-inferno", n = 4, contrast = c(0, 0.5)
@@ -33,9 +33,38 @@ Mypal <- c('#000000','#fca311','#e5383b','#e71d36')
 Mypal <- c('#000000','#fca311','#bf0603','#8d0801')
 
 tm_shape(msoa_map) +
-  tm_polygons("rate", style ="cont", palette = "-inferno",
+  tm_polygons("rate", style ="cont", palette = "-inferno", n = 4,
               border.alpha = 0) +
   tm_text("msoa_names", remove.overlap = TRUE, size = 0.8) + 
   tm_layout(legend.outside = TRUE, 
             frame = FALSE, frame.lwd = NA, panel.label.bg.color = NA)
 
+tm_shape(msoa_map) +
+  tm_polygons("rate_65_69", style ="cont", palette = "-inferno",
+              breaks = c(0, 0.2, 0.4, 0.6, 0.8, 1),
+              border.alpha = 0) +
+  tm_text("msoa_names", remove.overlap = TRUE, size = 0.8) + 
+  tm_layout(legend.outside = TRUE, 
+            frame = FALSE, frame.lwd = NA, panel.label.bg.color = NA)
+
+tm_shape(msoa_map) +
+  tm_polygons("rate_70_74", style ="cont", palette = "-inferno",
+              breaks = c(0, 0.2, 0.4, 0.6, 0.8, 1),
+              border.alpha = 0) +
+  tm_text("msoa_names", remove.overlap = TRUE, size = 0.8) + 
+  tm_layout(legend.outside = TRUE, 
+            frame = FALSE, frame.lwd = NA, panel.label.bg.color = NA)
+
+tm_shape(msoa_map) +
+  tm_polygons("rate_75_79", style ="cont", palette = "-inferno",
+              border.alpha = 0) +
+  tm_text("msoa_names", remove.overlap = TRUE, size = 0.8) + 
+  tm_layout(legend.outside = TRUE, 
+            frame = FALSE, frame.lwd = NA, panel.label.bg.color = NA)
+
+tm_shape(msoa_map) +
+  tm_polygons("rate_80", style ="cont", palette = "-inferno",
+              border.alpha = 0) +
+  tm_text("msoa_names", remove.overlap = TRUE, size = 0.8) + 
+  tm_layout(legend.outside = TRUE, 
+            frame = FALSE, frame.lwd = NA, panel.label.bg.color = NA)
